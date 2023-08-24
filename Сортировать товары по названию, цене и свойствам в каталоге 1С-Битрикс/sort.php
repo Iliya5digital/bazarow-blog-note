3b8e8629e831bac611037125dcaa1e964c0b3719
Iliya5digital <115736491+Iliya5digital@users.noreply.github.com>
2023-08-24 17:25:37 +0300

    Create sort.php

---
+++ Сортировать товары по названию, цене и свойствам в каталоге 1С-Битрикс/sort.php
@@ -0,0 +1,20 @@
+function process_form_data($data) {
+    // Обработка данных формы
+    if (empty($data['sortten'])) {
+        return false;
+    }
+    return $data['sortten'];
+}
+
+function set_cookie($name, $value, $expire = 0) {
+    // Установка куки
+    setcookie($name, $value, $expire, '/', '', false, true);
+}
+
+// Обработка данных формы
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $sortten = process_form_data($_POST);
+    if ($sortten !== false) {
+        set_cookie('sortten', $sortten);
+    }
+}
